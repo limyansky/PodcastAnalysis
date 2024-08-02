6a2a0012ddae5b1fb94fb7ce7553bcb1cd0d6f50
limyansky <brent.limyansky@ucsc.edu>
2024-08-02 12:47:05 -0400

    Found text to denote start-of-podcast

--- python/analysisScript.py
+++ python/analysisScript.py
@@ -12,6 +12,23 @@ import glob
 files = glob.glob('../Daddies_Transcripts/*.md')
 
 # sort by ep. number, assuming format "*Ep. [X OR XX] Episode Name"
+# might not handle Ep. 68 Pt. II
 files.sort(key = lambda x: int(x.split("Ep. ")[1].split(" ")[0]))
+
+# Marks the start of the non-scripted portion of the podcast
+intro_list = ["four dads from our world", 
+              "BDSM",
+              "\\[*intro music plays*\\]"]
+
+for file in files:
+    ep_number = int(file.split("Ep. ")[1].split(" ")[0])
+    
+    with open(file) as reading:
+        for line in reading:
+            if any(intro in line for intro in intro_list):
+                print(ep_number)
+                break
+            
+        
 
 
