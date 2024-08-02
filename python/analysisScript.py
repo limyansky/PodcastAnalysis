b1f305d59391c084b12bd32da1aa13fdba63febe
limyansky <brent.limyansky@ucsc.edu>
2024-08-02 11:43:55 -0400

    enumerate path to transcripts

---
+++ python/analysisScript.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 11:38:19 2024
+
+@author: brent
+"""
+
+import glob
+
+# transcript file paths
+files = glob.glob('../Daddies_Transcripts/*.md')
