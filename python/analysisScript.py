856057cca1241aa03b29b68809b9b893035c96c2
limyansky <brent.limyansky@ucsc.edu>
2024-08-03 21:47:53 -0400

    Parsing lines and preliminary statistics

--- python/analysisScript.py
+++ python/analysisScript.py
@@ -7,6 +7,7 @@ Created on Fri Aug  2 11:38:19 2024
 """
 
 import glob
+from collections import defaultdict
 
 # transcript file paths
 files = glob.glob('../Daddies_Transcripts/*.md')
@@ -20,15 +21,38 @@ intro_list = ["four dads from our world",
               "BDSM",
               "Welcome to the conclusion of season one"]
 
+# Holds the number of speaker occurances
+speaker_count = defaultdict(lambda:0)
+episode_count = defaultdict(lambda:0)
+length_one    = defaultdict(lambda:0) # A lot of sound effects. Finishing sentences.
+
 for file in files:
     ep_number = int(file.split("Ep. ")[1].split(" ")[0])
     
     with open(file) as reading:
+        in_body = False
         for line in reading:
+            if in_body:
+                line = line.replace("*","")
+                line = line.strip()
+                line = line.split(':')
+                if len(line) == 1:
+                    length_one[line[0]]+=1
+                elif len(line) == 2:
+                    speaker_count[line[0]]+=1
+                    episode_count[ep_number]+=1
             if any(intro in line for intro in intro_list):
-                print(ep_number)
-                break
-            
-        
+                in_body=True
 
+#speaker_count = dict(sorted(speaker_count.items(), key=lambda item: 1/item[1]))
 
+# Count how many interruptions we are missing by not counting double lines
+counter = 0
+for key in length_one.keys():
+    try:
+        if key[-1]=='—':
+            print(key)
+            counter+=1
+    except:
+        pass
+print(counter)
