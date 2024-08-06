be6a49a7bee037793c9c92c3499e0fe3c0c20a39
limyansky <brent.limyansky@ucsc.edu>
2024-08-06 12:20:59 -0400

    Add actor name to transcript

---
+++ python/transformTranscript.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  6 11:16:53 2024
+
+@author: brent
+"""
+
+import pandas as pd
+from collections import defaultdict
+import numpy as np
+
+df_raw = pd.read_csv("/home/brent/Documents/github/PodcastAnalysis/Data_Products/Intermediate/raw_NoIntro.csv")
+
+
+# Good for Diagnostics
+speaker_counts = df_raw.groupby(['Speaker'])['Speaker'].count().reset_index(name="Lines")
+speaker_counts.sort_values(by="Lines", inplace=True, ascending=False)
+
+# Translation Dictionary
+speaker_key = {
+    "Anthony":"Anthony",
+    "Matt":"Matt",
+    "Darryl":"Matt",
+    "Freddie":"Freddie",
+    "Glenn":"Freddie",
+    "Will":"Will",
+    "Henry":"Will",
+    "Beth":"Beth",
+    "Ron":"Beth",
+    "Jimmy":"Jimmy",
+    "Jodie":"Jimmy",
+    "Jenna":"Jenna",
+    "Ratticus":"Jenna",
+    "Ashly":"Ashly",
+    "Dennis":"Ashly",
+    "Mark":"Ashly",
+    "Juror":"Juror"
+    }
+
+speaker_dict = defaultdict(lambda:"Anthony", speaker_key)
+
+df_raw.insert(2, "Actor", [speaker_dict[x] for x in df_raw["Speaker"]])
+df_raw["Actor"][df_raw["Speaker"].isnull()] = np.nan
+
+df_raw.to_csv("/home/brent/Documents/github/PodcastAnalysis/Data_Products/Intermediate/actors_NoIntro.csv")
+
+actor_counts = df_raw.groupby(['Actor'])['Actor'].count().reset_index(name="Lines")
+actor_counts.sort_values(by="Lines", inplace=True, ascending=False)
